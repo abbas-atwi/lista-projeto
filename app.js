956754dd8e02d6f16532e2987407cf7ca12c6a9c
Abbas Atwi <abbas_9589_new@hotmail.com>
2023-07-10 23:38:58 -0300

    update

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const sectionlista = document.querySelectorAll(".section-box");
 function addCardCreate(classe, txt) {
   const card = document.createElement("div");
   const h1 = document.createElement("h1");
+  const h3 = document.createElement("h3");
   h1.innerHTML = txt;
   card.append(h1);
   card.classList.add(classe);
-
+  h3.innerHTML = ` (${h1.innerHTML}) Contem ${h1.innerHTML.length} caracteres`;
+  sectionBody.append(h3);
   sectionBody.appendChild(card);
 }
 function valueInput() {
